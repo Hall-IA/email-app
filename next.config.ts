6f32d5c045d3340efffd529d430e1e2d108eb20f
ASSANI Younes <younesassani@gmail.com>
2025-11-25 15:27:07 +0100

    ajout page notFound

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    globalNotFound: true,
+  },
   // Pas besoin de 'standalone' - le plugin Netlify gère automatiquement
 };
 
